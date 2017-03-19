a06e0301d34347910c97c3e66a1642e2e71a68ee
hd910 <hd910@outlook.com>
2017-03-19 21:34:03 +1300

    Get User Location

---
+++ app/src/main/java/hd/dataalert/LocationService.java
@@ -0,0 +1,119 @@
+package hd.dataalert;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.ServiceConfigurationError;
+
+/**
+ * Created by Hayde on 19-Mar-17.
+ */
+
+public class LocationService extends Service implements LocationListener {
+
+    private LocationManager locationManager;
+    private LocalBroadcastManager broadcastManager;
+
+    static final public String LOCATION_RESULT = "com.hd.dataalert.location";
+    static final public String LOCATION_MESSAGE = "com.hd.dataalert.locationMessage";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        broadcastManager = LocalBroadcastManager.getInstance(this);
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, final int flags, final int startId) {
+        if (intent.getAction().equals("startListening")) {
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                ActivityCompat.requestPermissions(activity,
+//                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                        255);
+            }else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+            }
+        }
+        else {
+            if (intent.getAction().equals("stopListening")) {
+                locationManager.removeUpdates(this);
+                locationManager = null;
+            }
+        }
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        try {
+            displayLocation(location);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void displayLocation(Location location) throws IOException {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> address = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        String locationAddress ="";
+        if(address.size() > 0){
+            locationAddress = address.get(0).getAddressLine(0)+ " " + address.get(0).getAddressLine(1);
+        }
+
+        Log.v("///////", locationAddress);
+        sendResult(locationAddress);
+
+    }
+
+
+    public void sendResult(String message) {
+        Intent intent = new Intent(LOCATION_RESULT);
+        if(message != null)
+            intent.putExtra(LOCATION_MESSAGE, message);
+        broadcastManager.sendBroadcast(intent);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
--- app/src/main/java/hd/dataalert/MainActivity.java
+++ app/src/main/java/hd/dataalert/MainActivity.java
@@ -1,17 +1,34 @@
 package hd.dataalert;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver receiver;
+    private TextView locationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +37,65 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        locationTextView = (TextView)findViewById(R.id.locationText);
+
+        Button clickButton = (Button) findViewById(R.id.getGPSBtn);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                locationTextView.setText("");
+                Intent intent = new Intent(MainActivity.this, LocationService.class);
+                intent.setAction("startListening");
+                startService(intent);
+            }
+        });
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-                Snackbar.make(view, wifi.isWifiEnabled()?"Wifi is Enabled": "Wifi is Off", Snackbar.LENGTH_LONG)
+                boolean wifiNeeded = checkWifiStatus();
+                Snackbar.make(view, wifiNeeded ? "Wifi is Enabled" : "Wifi is Off", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                checkWifiStatus();
             }
         });
+
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String s = intent.getStringExtra(LocationService.LOCATION_MESSAGE);
+                locationTextView.setText(s);
+
+                Intent i = new Intent(MainActivity.this, LocationService.class);
+                i.setAction("stopListening");
+                startService(i);
+            }
+        };
+    }
+
+    private boolean checkWifiStatus() {
+        WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+        return wifi.isWifiEnabled();
+    }
+
+    @Override
+    protected void onStop() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(this).registerReceiver((receiver),
+                new IntentFilter(LocationService.LOCATION_RESULT)
+        );
     }
 
     @Override
